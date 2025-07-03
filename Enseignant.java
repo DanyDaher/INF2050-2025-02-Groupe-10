826b4f6998d614583f8957d3ac2ff47550021075
Dany Daher <danydaher410@gmail.com>
2025-07-03 17:25:36 -0400

    Ajout de getters, setters, add, et constructeurs pour les classes Enseignant.java et EnseignantImpl.java

--- Enseignant.java
+++ Enseignant.java
@@ -2,4 +2,28 @@ public class Enseignant {
     public String codeenseignant;
     public String nom;
     public String prenom;
+
+    public Enseignant(String codeenseignant, String nom, String prenom) {
+        this.codeenseignant = codeenseignant;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+    public String getCodeenseignant() {
+        return codeenseignant;
+    }
+    public void setCodeenseignant(String codeenseignant) {
+        this.codeenseignant = codeenseignant;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 }
---
+++ EnseignantImpl.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EnseignantImpl extends Enseignant {
+    private final List<Enseignant> enseignants = new ArrayList<>();
+
+    public EnseignantImpl(String codeenseignant, String nom, String prenom, List<Enseignant> enseignants) {
+        super(codeenseignant, nom, prenom);
+        this.enseignants.addAll(enseignants);
+    }
+    public void addEnseignant(Enseignant e) {
+        enseignants.add(e);
+    }
+    public Enseignant getEnseignants(String codeenseignant) {
+        for (Enseignant e : enseignants) {
+            if (e.getCodeenseignant().equals(codeenseignant)) {
+                return e;
+            }
+        }
+        return null;
+    }
+}
