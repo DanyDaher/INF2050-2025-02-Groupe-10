b695c73a7b53b2fdb44f041291e9cf25638ab476
Dany Daher <danydaher410@gmail.com>
2025-07-07 11:49:55 -0400

    Recuperation de version: Toutes les classes

--- src/main/java/ca/uqam/inf2050/Etudiant.java
+++ src/main/java/ca/uqam/inf2050/Etudiant.java
@@ -1,15 +1,38 @@
-package src.main.java.ca.uqam.inf2050;
-
 public class Etudiant {
     public String codepermanent;
     public String nom;
     public String prenom;
     public Number codeprogramme;
 
-    public Etudiant(String codepermanent,String nom, String prenom, Number codeprogramme){
+    public Etudiant(String codepermanent, String nom, String prenom, Number codeprogramme){
         this.codepermanent=codepermanent;
         this.nom=nom;
         this.prenom=prenom;
         this.codeprogramme=codeprogramme;
+    }
+
+    public String getCodePermanent() {
+        return codepermanent;
+    }
+    public void setCodePermanent(String codepermanent) {
+        this.codepermanent = codepermanent;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public String getPrenom() {
+        return prenom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public Number getCodeProgramme() {
+        return codeprogramme;
+    }
+    public void setCodeProgramme(Number codeprogramme) {
+        this.codeprogramme = codeprogramme;
     }
 }
