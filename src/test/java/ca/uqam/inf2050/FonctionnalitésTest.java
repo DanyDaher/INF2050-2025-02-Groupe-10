4f310664e3cfbdc9f1998b1c14d5076c8f1b3612
Dany Daher <danydaher410@gmail.com>
2025-07-10 19:00:40 -0400

    Ajout tests testGetTousCoursSessionEnCours() et testGetProgrammePourcentageEleveSessionEnCours()

--- src/test/java/ca/uqam/inf2050/FonctionnalitésTest.java
+++ src/test/java/ca/uqam/inf2050/FonctionnalitésTest.java
@@ -25,6 +25,15 @@ public class Fonctionnalité3et4Test {
         service = new Fonctionnalité3et4();
         groupecours = new Groupecours();
         inscriptions = new ArrayList<>();
+        Etudiant E1 = new Etudiant("ASD33","Ef","Bal",100);
+        Etudiant E2 = new Etudiant("DEF13","Com","Los",101);
+        Cours C1 = new Cours("INF1133", "Base de données", "Introduction à plusieurs types de bases de données", 3);
+        Cours C2 = new Cours("INF1144", "IT", "Introduction à la technologie de l'information", 3);
+        Session S1 = new Session("E2025", "04/24/2025", null);
+        Enseignant E1 = new Enseignant("ISBA222", "Bandeau","Ismael");
+        Enseignant E2 = new Enseignant("ISBA222", "Marano", "Juliette");
+        Groupecours G1 = new Groupecours(C1, E1, S1, 50, "AS-25");
+        Inscription I1 = new Inscription(E1, G1, "04/24/2025", null, 80);
     }
 
     @Test
@@ -53,6 +62,21 @@ public class Fonctionnalité3et4Test {
 
     @Test
     public void testGetTousCoursSessionEnCours(){
+        List<Inscription> inscriptions = new ArrayList<>();
+        inscriptions.add(I1);
+        InscriptionImpl Ins1 = new InscriptionImpl(E1, G1, "04/24/2025", null, 80, inscriptions);
+        l1 = Ins1.getTousCoursSessionEnCours(101);
+        l2 = Ins1.getTousCoursSessionEnCours(101);
+        assertArrayEquals(l1, l2);
+        assertSame(l1,l2);
+    }
 
+    @Test
+    public void testGetProgrammePourcentageEleveSessionEnCours(){
+        List<Inscription> inscriptions = new ArrayList<>();
+        inscriptions.add(I1);
+        InscriptionImpl Ins1 = new InscriptionImpl(E1, G1, "04/24/2025", null, 80, inscriptions);
+        p1 = Ins1.getProgrammePourcentageEleveSessionEnCours();
+        assertThat(p1 == 1/50);
     }
 }
